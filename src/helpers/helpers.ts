38dca7736469f8d60f774e08f89949d998c58afe
Vincent Lamanna <vincent@crewdle.com>
2024-03-25 16:57:46 -0400

    Add documentation for helpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,5 +1,11 @@
 import type { IDatabaseTableQuery, IValueType } from '@crewdle/web-sdk-types';
 
+/**
+ * Orders the values based on the query
+ * @param values The values to order
+ * @param query The query
+ * @ignore
+ */
 export function orderValues<T extends IValueType>(values: T[], query?: IDatabaseTableQuery): void {
   if (query?.orderBy) {
     values.sort((a: any, b: any) => {
@@ -20,6 +26,13 @@ export function orderValues<T extends IValueType>(values: T[], query?: IDatabase
   }
 }
 
+/**
+ * Checks if the value matches the query
+ * @param value The value to check
+ * @param query The query
+ * @returns True if the value matches the query
+ * @ignore
+ */
 export function isMatchingQuery<T extends IValueType>(value: T, query?: IDatabaseTableQuery): boolean {
   if (query?.where) {
     const anyValue = value as any;
