d1ecfc9358917166fd6cb9088f45957682cb0924
Vincent Lamanna <vincent@crewdle.com>
2024-03-25 16:58:23 -0400

    Add documentation to types

--- dist/types/helpers/helpers.d.ts
+++ dist/types/helpers/helpers.d.ts
@@ -1,3 +1,16 @@
 import type { IDatabaseTableQuery, IValueType } from '@crewdle/web-sdk-types';
+/**
+ * Orders the values based on the query
+ * @param values The values to order
+ * @param query The query
+ * @ignore
+ */
 export declare function orderValues<T extends IValueType>(values: T[], query?: IDatabaseTableQuery): void;
+/**
+ * Checks if the value matches the query
+ * @param value The value to check
+ * @param query The query
+ * @returns True if the value matches the query
+ * @ignore
+ */
 export declare function isMatchingQuery<T extends IValueType>(value: T, query?: IDatabaseTableQuery): boolean;
